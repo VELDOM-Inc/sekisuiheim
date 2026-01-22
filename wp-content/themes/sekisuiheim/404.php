05d293ed070610b8a7f6c3a526728a071cf4137e
smatsushima1 <s.matsushima@veldom.co.jp>
2026-01-22 18:27:51 +0900

    全データ再アップ

--- wp-content/themes/sekisuiheim/404.php
+++ wp-content/themes/sekisuiheim/404.php
@@ -1,18 +1,14 @@
 <?php get_header(); ?>
 
-    <div class="c-mv">
-		<h1 class="c-mv__inner">WPテンプレート-404エラー</h1>
+    <div class="c-mv error-mv">
+		<h1 class="c_ttl is-fade" data-en="">404 <br class="u-sm-max">NOT FOUND</h1>
 	</div>
 
-    <div class="c-breadcrumb">
-		<div class="c-breadcrumb__inner">
-			<a class="c-breadcrumb__item" href="<?php echo home_url(); ?>">TOP</a>
-        	<span class="c-breadcrumb__item">お探しのページが見つかりません</span>
-		</div>
-    </div>
-
     <section class="c-error">
-        <p class="c-error__txt">お探しのページは一時的に<br class="u-sm-max">アクセスができない状況にあるか、<br>移動もしくは削除された可能性があります。<br>お手数ではございますが、<br class="u-sm-max">トップにて目的のページをお探しください。</p>
+        <h2 class="c_ttl">お探しのページが見つかりません</h2>
+        <p class="c_txt">お探しのページは一時的に<br class="u-sm-max">アクセスができない状況にあるか、<br>
+        移動もしくは削除された可能性があります。<br>
+        お手数ではございますが、<br class="u-sm-max">TOPにて目的のページをお探しください。</p>
         <a class="c_btn" href="<?php echo home_url(); ?>">TOPへ戻る</a>
     </section>
 
